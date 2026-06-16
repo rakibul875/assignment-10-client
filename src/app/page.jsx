6398bbcd95ccd7bb31807e939ca36da6f5c0cecb
rakibul875 <rakibulislamashik78@gmail.com>
2026-06-16 19:38:57 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button variant="primary">
+        My Button
+      </Button>
+    </div>
+  );
+};
+export default HomePage;
